a126782ed88a2eeca2e5e216546ab6bd00ce2266
Jurvis Tan <hello@jurvis.co>
2022-09-05 11:09:11 -0700

    Simplify BitcoinCoreRpcConfig interface

--- Lightning/Sources/Lightning/Chain Sync/Bitcoin Core/BitcoinCoreRpcConfig.swift
+++ Lightning/Sources/Lightning/Chain Sync/Bitcoin Core/BitcoinCoreRpcConfig.swift
@@ -8,15 +8,8 @@
 import Foundation
 
 public struct BitcoinCoreRpcConfig {
-    let rpcUsername: String
-    let rpcPassword: String
-    let rpcPort: UInt
-    let rpcDomain: String
-    
-    public init(rpcUsername: String, rpcPassword: String, rpcPort: UInt, rpcDomain: String) {
-        self.rpcUsername = rpcUsername
-        self.rpcPassword = rpcPassword
-        self.rpcPort = rpcPort
-        self.rpcDomain = rpcDomain
-    }
+    let username: String
+    let password: String
+    let port: UInt
+    let host: String
 }
