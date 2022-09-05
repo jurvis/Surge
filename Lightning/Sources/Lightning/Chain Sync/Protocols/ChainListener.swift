f4c98bc9712b8f67326724c69ea2e0ffcac87761
Jurvis Tan <hello@jurvis.co>
2022-09-05 12:09:19 -0700

    Implement ChainListener
    Wraps both ChannelManager and ChainMonitor so we can tell it when blocks
    are connected

--- Lightning/Sources/Lightning/Chain Sync/Protocols/ChainListener.swift
+++ Lightning/Sources/Lightning/Chain Sync/Protocols/ChainListener.swift
@@ -6,8 +6,26 @@
 //
 
 import Foundation
+import LightningDevKit
 
-public protocol ChainListener {
-    func blockConnected(block: [UInt8], height: UInt32);
-    func blockDisconnected(header: [UInt8]?, height: UInt32);
+class ChainListener {
+    private let channelManager: ChannelManager
+    private let chainMonitor: ChainMonitor
+
+    init(channelManager: ChannelManager, chainMonitor: ChainMonitor) {
+        self.channelManager = channelManager
+        self.chainMonitor = chainMonitor
+    }
+    
+    func blockConnected(block: [UInt8], height: UInt32) {
+        print("block connected at height \(height): \(block)")
+        self.channelManager.as_Listen().block_connected(block: block, height: height)
+        self.chainMonitor.as_Listen().block_connected(block: block, height: height)
+    }
+
+    func blockDisconnected(header: [UInt8]?, height: UInt32) {
+        print("block disconnected from height \(height): \(header)")
+        self.channelManager.as_Listen().block_disconnected(header: header, height: height)
+        self.chainMonitor.as_Listen().block_disconnected(header: header, height: height)
+    }
 }
